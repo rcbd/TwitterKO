4c0b3bc2ab9eaa57f884fec47d622127f436b735
robin <robincurts@gmail.com>
2016-08-10 21:36:52 -0400

    Committing all files - first commit.

---
+++ TwitterKO/Controllers/TweetController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TwitterKO.Models;
+
+namespace TwitterKO.Controllers
+{
+    public class TweetController : ApiController
+    {
+        public IEnumerable<Tweet> Get(string query)
+        {
+            var tweets = new List<Tweet>();
+
+            var i = 0;
+            while (i < 10)
+            {
+                tweets.Add(new Tweet {Message = Guid.NewGuid().ToString()});
+                i++;
+            }
+
+            return tweets;
+        }
+
+    }
+}
---
+++ TwitterKO/Models/TwitterSearchModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TwitterKO.Models
+{
+    public class TwitterSearchModel
+    {
+
+        public static IEnumerable<string> GetTweets(string query)
+        {
+            return new String[]{ "foo", "bar" }.ToList();
+        }
+
+    }
+}
